a02aec7fa126760a38891c96e3a838ab1c61f85c
supergajit2 <supergajit1@comcast.net>
2021-02-18 14:07:50 -0700

    finished config and ran basic test

---
+++ build/tests/example.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Config test', function () {
+    it('should work', function () {
+        console.log("Wahoo");
+    });
+});
---
+++ src/tests/example.test.js
@@ -0,0 +1,5 @@
+describe('Config test', () => {
+    it('should work', () => {
+        console.log("Wahoo")
+    })
+})
